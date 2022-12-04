c5139d1bbc091258a65f99382fbb7bef49e6c62d
Attila Horvath <atiradeon86@hotmail.com>
2022-12-04 23:48:08 +0100

    V03

--- deploy.ps1
+++ deploy.ps1
@@ -4,7 +4,7 @@ $Title = 'Bryan Deplyoment Solutions - Always Look on the Bright Side of Life ..
     Clear-Host
     Write-Host "======= $Title =======`r`n" -ForegroundColor Green
     
-    Write-Host "1: Press '1' for Deploy DC1 (Active Directory Domain Controller - project.local )"
+    Write-Host "1: Press '1' for Deploy DC1 (WS 2019 Active Directory Domain Controller - project.local )"
     Write-Host "2: Press '2' for Deploy FS1 (Windows Server 2019 Core - File Server)" 
     Write-Host "3: Press '3' for Deploy Windows 10 Client (21H2-Pro)"
     Write-Host "4: Press '4' for Deploy Windows 11 Client (21H2-Pro)"
