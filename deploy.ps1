7552cb06fcc434280a2ed78289bf5ac616a9c0e4
Attila Horvath <atiradeon86@hotmail.com>
2022-12-04 23:50:52 +0100

    V03

--- deploy.ps1
+++ deploy.ps1
@@ -8,7 +8,7 @@ $Title = 'Bryan Deplyoment Solutions - Always Look on the Bright Side of Life ..
     Write-Host "2: Press '2' for Deploy FS1 (Windows Server 2019 Core - File Server)" 
     Write-Host "3: Press '3' for Deploy Windows 10 Client (21H2-Pro)"
     Write-Host "4: Press '4' for Deploy Windows 11 Client (21H2-Pro)"
-    Write-Host "5: Press '5' for Auto Deploy`r`n" -ForegroundColor Red
+    Write-Host "5: Press '5' for Auto Deploy (DC1 + FS1 + W10 Client)`r`n" -ForegroundColor Red
     Write-Host "Q: Press 'Q' to quit.`r`n"
 
 $menu = Read-Host "What you want to do?"
